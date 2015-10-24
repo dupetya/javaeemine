e8d8141824a41fe409b91205209d22fe72d226fc
Péter Dudás <dupetya@gmail.com>
2015-10-24 19:48:55 +0200

    Deleted project for refactoring

--- portal-app/src/main/java/hu/schonherz/training/java/solid/cipher/CipherService.java
+++
@@ -1,5 +0,0 @@
-package hu.schonherz.training.java.solid.cipher;
-
-public interface CipherService {
-  String encrypt(String unencrypted) throws EncryptionException;
-}
--- portal-app/src/main/java/hu/schonherz/training/java/solid/converter/Converter.java
+++
@@ -1,5 +0,0 @@
-package hu.schonherz.training.java.solid.converter;
-
-public interface Converter<S, T> {
-  T convert(S source) throws ConversionException;
-}
